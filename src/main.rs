08c7b22ec08c9f4b1067b668c79e39548667f418
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-03-12 23:10:22 +0000

    Add some setup and cleanup functions

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,40 @@
-use std::io::{Write, stdout};
+use crossterm::{ExecutableCommand, cursor, terminal};
 
-use crossterm::{
-    ExecutableCommand, event, execute,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-};
+/// All the state and main methods for the TUI program
+struct Paint2D {
+    stdout: std::io::Stdout,
+}
+
+impl Paint2D {
+    fn new() -> Self {
+        Paint2D {
+            stdout: std::io::stdout(),
+        }
+    }
+
+    fn setup(&mut self) -> std::io::Result<()> {
+        self.stdout.execute(terminal::EnterAlternateScreen)?;
+        self.stdout.execute(cursor::Hide)?;
+        terminal::enable_raw_mode()?;
+        Ok(())
+    }
+
+    fn run(&mut self) -> std::io::Result<()> {
+        loop {}
+    }
+}
+
+impl Drop for Paint2D {
+    fn drop(&mut self) {
+        let _ = terminal::disable_raw_mode();
+        let _ = self.stdout.execute(cursor::Show);
+        let _ = self.stdout.execute(terminal::LeaveAlternateScreen);
+    }
+}
 
 fn main() -> std::io::Result<()> {
-    // using the macro
-    execute!(
-        stdout(),
-        SetForegroundColor(Color::Blue),
-        SetBackgroundColor(Color::Red),
-        Print("Styled text here."),
-        ResetColor
-    )?;
-
-    // or using functions
-    stdout()
-        .execute(SetForegroundColor(Color::Blue))?
-        .execute(SetBackgroundColor(Color::Red))?
-        .execute(Print("Styled text here."))?
-        .execute(ResetColor)?;
-
+    let mut app = Paint2D::new();
+    app.setup()?;
+    app.run()?;
     Ok(())
 }
