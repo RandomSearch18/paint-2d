e567f9af3c1813abcbc2077238ee5abc5e2f92ee
RandomSearch <101704343+RandomSearch18@users.noreply.github.com>
2025-03-12 22:49:36 +0000

    + crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Paint 2D!");
+use std::io::{Write, stdout};
+
+use crossterm::{
+    ExecutableCommand, event, execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+};
+
+fn main() -> std::io::Result<()> {
+    // using the macro
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Styled text here."),
+        ResetColor
+    )?;
+
+    // or using functions
+    stdout()
+        .execute(SetForegroundColor(Color::Blue))?
+        .execute(SetBackgroundColor(Color::Red))?
+        .execute(Print("Styled text here."))?
+        .execute(ResetColor)?;
+
+    Ok(())
 }
